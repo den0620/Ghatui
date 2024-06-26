26a24ffd55edf34f5034391aaa0f461528a8af72
den0620 <denis2006d@gmail.com>
2024-06-26 06:33:52 +0500

    only websoc read doesnt work for some reason

--- client/client.go
+++ client/client.go
@@ -1,32 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
+	"fmt"
+	"os"
+	"strings"
 	"time"
 	"sync"
 
     "github.com/gorilla/websocket"
 
-    "github.com/charmbracelet/bubbles/textarea"
-    "github.com/charmbracelet/bubbles/textinput"
-    "github.com/charmbracelet/bubbles/viewport"
-    tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 type state int
 const (
     stateLogin state = iota
     stateInvite
+    stateInviteReceived
     stateChat
 )
 var (
     conn          *websocket.Conn
-    validUsername string
-    validPassword string
     mate          string
-    OnlineUsers   string
     message       = make(chan Message, 1)
     chatting      = make(chan bool, 1)
 )
@@ -46,75 +44,96 @@ type model struct {
     focusIndex    int
     quitting      bool
     err           string
+    inviteFrom    string
 }
 func (m model) Init() tea.Cmd {
-    return textarea.Blink
+	return textarea.Blink
 }
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg := msg.(type) {
     case tea.KeyMsg:
         switch msg.Type {
-	case tea.KeyTab:
-	    if m.state == stateLogin {
-	        m.focusIndex++
-	        if m.focusIndex > 1 {
-		    m.focusIndex = 0
-	        }
-	        if m.focusIndex == 0 {
-	            m.usernameInput.Focus()
-		    m.passwordInput.Blur()
-	        } else {
-		    m.usernameInput.Blur()
-		    m.passwordInput.Focus()
-	        }
-	    }
-	case tea.KeyEnter:
-	    if m.state == stateLogin {
-		    send("Register", []string{m.usernameInput.Value(), m.passwordInput.Value()})
-		    mess := <-message
-		    if mess.Type == "UserExist" {
-		    	send("Login", []string{m.usernameInput.Value(), m.passwordInput.Value()})
-		    	mess = <-message
+		case tea.KeyTab:
+		    if m.state == stateLogin {
+		        m.focusIndex++
+		        if m.focusIndex > 1 {
+			    	m.focusIndex = 0
+		        }
+		        if m.focusIndex == 0 {
+		            m.usernameInput.Focus()
+			    	m.passwordInput.Blur()
+		        } else {
+			    	m.usernameInput.Blur()
+			    	m.passwordInput.Focus()
+	        	}
+	    	}
+		case tea.KeyEnter:
+		    if m.state == stateLogin {
+			    send("Register", []string{m.usernameInput.Value(), m.passwordInput.Value()})
+			    mess := <-message
+			    if mess.Type == "UserExist" {
+			    	send("Login", []string{m.usernameInput.Value(), m.passwordInput.Value()})
+			    	mess = <-message
+			    }
+			    switch mess.Type {
+			    case "AlreadyOnline":
+					m.err = "User already online"
+					return m, nil
+				case "WrongPassword":
+					m.err = "Wrong password"
+					return m, nil
+				case "Logged", "Registered":
+				    m.state = stateInvite
+				    m.chatInput.Focus()
+				}
+		    } else if m.state == stateInvite {
+				send("Invite", m.chatInput.Value())
+				m.chatInput.SetValue("")
+			} else if m.state == stateInviteReceived {
+				if m.chatInput.Value() == "y" || m.chatInput.Value() == "Y" {
+					send("InviteCrush", m.inviteFrom)
+					mate = m.inviteFrom
+					m.state = stateChat
+				} else {
+					send("InvitePass", m.inviteFrom)
+					m.state = stateInvite
+				}
+				m.chatInput.SetValue("")
+		    } else if m.state == stateChat {
+				m.messages = append(m.messages, m.chatInput.Value())
+				send("Chat", m.chatInput.Value())
+				m.chatInput.SetValue("")
 		    }
-		    switch mess.Type {
-		    case "AlreadyOnline":
-				m.err = "User already online"
-				return m, nil
-			case "WrongPassword":
-				m.err = "Wrong password"
-				return m, nil
-			case "Logged", "Registered":
-			    m.state = stateInvite
-			    m.chatInput.Focus()
-			}
-	    } else if m.state == stateInvite {
-		send("Invite", m.chatInput.Value())
-		m.chatInput.SetValue("")
-	    } else if m.state == stateChat {
-		m.messages = append(m.messages, m.chatInput.Value())
-		send("Chat", m.chatInput.Value())
-		m.chatInput.SetValue("")
-	    }
-	case tea.KeyCtrlC, tea.KeyEsc:
-	        return m, tea.Quit
-	}
-    case Message:
-        switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+		        return m, tea.Quit
+		}
+	case Message:
+	    switch msg.Type {
 		case "InviteRequest":
-	    	m.state = stateInvite
-	    	m.err = fmt.Sprintf("Invite from: %s", msg.Data)
+	    	m.state = stateInviteReceived
+	    	m.inviteFrom = msg.Data.(string)
+	    	m.err = fmt.Sprintf("Invite from: %s (y/n)", msg.Data)
+		case "InviteAccept":
+	    	mate = msg.Data.(string)
+	    	m.state = stateChat
+		case "InviteRefuse":
+	    	m.err = fmt.Sprintf("Invite refused by: %s", msg.Data)
 		case "Chat":
 	    	m.messages = append(m.messages, fmt.Sprintf("%s: %s", mate, msg.Data))
+		case "MateClosed":
+	    	m.err = "Your mate has left the chat."	
+	       	m.state = stateInvite
 		case "OnlineUsers":
-	    	OnlineUsers = msg.Data.(string)
-        }
+		   	m.onlineUsers = msg.Data.(string)
+		}
     }
     switch m.state {
     case stateLogin:
-	m.usernameInput, _ = m.usernameInput.Update(msg)
-	m.passwordInput, _ = m.passwordInput.Update(msg)
-    case stateInvite,stateChat:
-	m.chatInput, _ = m.chatInput.Update(msg)
+		m.usernameInput, _ = m.usernameInput.Update(msg)
+		m.passwordInput, _ = m.passwordInput.Update(msg)
+    case stateInvite, stateChat, stateInviteReceived:
+		m.chatInput, _ = m.chatInput.Update(msg)
     }
 
     return m, nil
@@ -132,8 +151,13 @@ func (m model) View() string {
         }
     case stateInvite:
 		b.WriteString("Online Users:\n\n")
-		b.WriteString(OnlineUsers + "\n")
+		b.WriteString(m.onlineUsers + "\n")
 		b.WriteString(fmt.Sprintf("Invite user to chat: %s\n", m.chatInput.View()))
+		if m.err != "" {
+			b.WriteString(fmt.Sprintf("\nNotice: %s\n", m.err))
+		}
+	case stateInviteReceived:
+		b.WriteString(fmt.Sprintf("Invite from %s. Accept? (y/n): %s\n", m.inviteFrom, m.chatInput.View()))
 		if m.err != "" {
 			b.WriteString(fmt.Sprintf("\nNotice: %s\n", m.err))
 		}
@@ -170,9 +194,9 @@ func initialModel() model {
 
     return model{
     	usernameInput: ui,
-	passwordInput: pi,
-	chatInput:     ci,
-	state:         stateLogin,
+		passwordInput: pi,
+		chatInput:     ci,
+		state:         stateLogin,
     	quitting:      false,
     }
 }
@@ -202,15 +226,22 @@ func main() {
     	os.Exit(1)
     }
 
+    go func() { // read
+		for msg := range message {
+			p.Send(msg)
+			fmt.Print(msg)
+		}
+	}()
+
     wg.Wait()
 }
 
 func read(conn *websocket.Conn) {
+	var msg Message
 	for {
-		var msg Message
-		if err := conn.ReadJSON(&msg); err != nil {
-			fmt.Println("Error reading from server:", err)
-			return
+		if conn.ReadJSON(&msg) != nil {
+			fmt.Print("Error reading from server: ", msg, "\n")
+			conn.Close()
 		}
 		message <- msg
 	}
